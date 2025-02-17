d1d29ba2dd80030b5da8f338457b9105a550644f
Knyazew36 <knyazew.sv@gmail.com>
2025-02-17 19:50:00 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bitrix24')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('bitrix')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
